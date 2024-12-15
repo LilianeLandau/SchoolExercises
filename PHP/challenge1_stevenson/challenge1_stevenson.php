425a327e76ca2b8a5331823d26830a2f11acb576
LilianeLandau <landauliliane@hotmail.fr>
2024-12-15 15:15:14 +0100

    send files PHP, PHP OBJET, SQL, JAVASCRIPT

---
+++ PHP/challenge1_stevenson/challenge1_stevenson.php
@@ -0,0 +1,18 @@
+<?php
+
+echo $titre="Voyage avec un âne dans les Cévennes"."<br/>";
+echo $livreLu=True."<br/>";
+echo $anParutionLivre=2017 ."<br/>";
+echo $prixLivre=6.20."<br/>";
+
+
+$firstname = 'Robert';
+$lastname = 'Stevenson';
+
+echo $firstname . $lastname;  // affiche : RobertStevenson
+
+$presentation = "$firstname $lastname est un célèbre archéologue"; // les variables sont interprétées 
+echo $presentation; // affiche : Indiana Jones est un célèbre archéologue
+
+
+?>
